3363ab2dbddf6d076ea584a15f5705a61f3e8109
Your Name <you@example.com>
2020-10-16 09:00:00 -0700

    Added view models for vertex and connection

---
+++ ConnectionViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTSP_2
+{
+    class ConnectionViewModel : INotifyPropertyChanged
+    {
+        private VertexViewModel v1 = null;
+        private VertexViewModel v2 = null;
+
+        public ConnectionViewModel(VertexViewModel v1, VertexViewModel v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public VertexViewModel V1
+        {
+            get => v1;
+            set
+            {
+                v1 = value;
+                OnPropertyChanged("V1");
+            }
+        }
+
+        public VertexViewModel V2
+        {
+            get => v2;
+            set
+            {
+                v2 = value;
+                OnPropertyChanged("V2");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
---
+++ VertexViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GTSP_2
+{
+    class VertexViewModel : INotifyPropertyChanged
+    {
+        private double x = 0, y = 0;
+        private double width = 0, height = 0;
+        private Color color;
+
+        public VertexViewModel(double x, double y, double width, double height, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.color = color;
+        }
+
+        public double X
+        {
+            get => x;
+            set
+            {
+                if (x != value)
+                {
+                    x = value;
+                    OnPropertyChanged("X");
+                }
+            }
+        }
+
+        public double Y
+        {
+            get => y; 
+            set
+            {
+                if (y != value)
+                {
+                    y = value;
+                    OnPropertyChanged("Y");
+                }
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (width != value)
+                {
+                    width = value;
+                    OnPropertyChanged("Width");
+                }
+            }
+        }
+
+        public double Height
+        {
+            get => height; 
+            set
+            {
+                if (height != value)
+                {
+                    height = value;
+                    OnPropertyChanged("Height");
+                }
+            }
+        }
+
+        public Color Color
+        {
+            get => color;
+            set
+            {
+                if (color != value)
+                {
+                    color = value;
+                    OnPropertyChanged("Color");
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
